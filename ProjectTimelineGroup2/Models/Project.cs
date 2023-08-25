1ccc97b2d336612a1d9390df7e16a62e3235fcfb
ST10069206 <st10069206@vcconnect.edu.za>
2023-08-25 15:48:39 +0200

    Created Project class and added properties and overloaded constructor

---
+++ ProjectTimelineGroup2/Models/Project.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTimelineGroup2.Models
+{
+    public class Project
+    {
+        public  string ProjectCode { get; set; }
+        public string ProjectName { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set;}
+
+        public int Duration { get; set; }
+        public double EstimatedCost { get; set; }
+
+        public Project(string projectCode, string projectName, DateOnly startDate, DateOnly endDate,int duration,double estimatedCost)
+        {
+            ProjectCode = projectCode;
+            ProjectName = projectName;
+            StartDate = startDate;
+            EndDate = endDate;
+            Duration = duration;
+            EstimatedCost = estimatedCost;
+        }
+
+    }
+}
